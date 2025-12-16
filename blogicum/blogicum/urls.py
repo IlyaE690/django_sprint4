455106b531f76eb1edad271a83576ffbeebaf715
Ilya <itchuprakov@yandex.ru>
2025-12-16 18:52:36 +0300

    fix not found error

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
 
 handler403 = 'blog.views.csrf_failure'
