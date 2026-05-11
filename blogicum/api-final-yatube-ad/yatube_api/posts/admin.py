1bd9a27ab10d538f0cca4cadeaf79832b96e464f
Ilya <itchuprakov@yandex.ru>
2026-05-11 20:51:23 +0300

    remove submodule

---
+++ blogicum/api-final-yatube-ad/yatube_api/posts/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Post, Group, Comment, Follow
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'created', 'author', 'post')
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'following')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    list_editable = ('group',)
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'created', 'author', 'post')
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'following')
